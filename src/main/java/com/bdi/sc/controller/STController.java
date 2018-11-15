c9f01cc0fef77f4e064dc756e17303e3bcf56305
KOITT <KOITT@KOITT-PC>
2018-11-15 19:50:02 +0900

    dd

--- src/main/java/com/bdi/sc/controller/STController.java
+++ src/main/java/com/bdi/sc/controller/STController.java
@@ -3,8 +3,11 @@ package com.bdi.sc.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bdi.sc.service.STService;
@@ -22,5 +25,17 @@ public class STController {
 	@GetMapping(value="/sts/{stnum}")
 	public @ResponseBody STVO selectST(@PathVariable int stnum) {
 		return sts.selectST(stnum);
+	}
+	@PostMapping(value="/sts")
+	public @ResponseBody int insertST(STVO st) {
+		return sts.insertST(st);
+	}
+	@PutMapping(value="/sts")
+	public @ResponseBody int updateST(STVO st) {
+		return sts.updateST(st);
+	}
+	@DeleteMapping(value="/sts/{stnum}")
+	public @ResponseBody int deleteST(@PathVariable int stnum) {
+		return sts.deleteST(stnum);
 	}
 }
