f4d59193f4a44ce4a2ac45839ebe7a3d5456178e
Administrator <Administrator@DESKTOP-HOOIS9P>
2018-12-04 14:04:06 +0900

    박상우 schoolVO

---
+++ src/main/java/com/bdi/sc/vo/SchoolInfo.java
@@ -0,0 +1,34 @@
+package com.bdi.sc.vo;
+
+import lombok.Data;
+
+@Data
+public class SchoolInfo {
+	 private int sinum;
+	 private String city_education;
+	 private String regional_education;
+	 private String regional;
+	 private String disclosure_code;
+	 private String school_name;
+	 private Integer school_code;
+	 private String establishment;
+	 private String school_characteristics;
+	 private String branch_division;
+	 private String establishment_type;
+	 private String weekly;
+	 private Integer school_anniversary;
+	 private Integer establishment_date;
+	 private Integer court_code;
+	 private String address;
+	 private String detailed_address;
+	 private Integer zip_code;
+	 private Integer roadname_code;
+	 private String roadname_address;
+	 private String roadname_detailed_address;
+	 private double latitude;
+	 private double longitude;
+	 private String phone_number;
+	 private String fax_number;
+	 private String homepage;
+	 private String coeducation_division;
+}
