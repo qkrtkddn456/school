d57e0fb5dea27605d88b65da62346dcdf9d092a3
Administrator <Administrator@DESKTOP-HOOIS9P>
2018-12-13 10:38:43 +0900

    박상우 studentinfo

---
+++ src/main/java/com/bdi/sc/vo/StudentInfo.java
@@ -0,0 +1,20 @@
+package com.bdi.sc.vo;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Alias("student")
+@Data
+public class StudentInfo {
+	
+	private Integer studentnum;
+	private String studentname;
+	private String studentid;
+	private String studentpwd;
+	private Integer studentage;
+	private Integer studentgender;
+	private String studentemail;
+	private Integer admin;
+	private Integer sinum;
+}
